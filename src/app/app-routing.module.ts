fa70607e849a3318a2c2334eb0f69910ee484512
Joshy Joy <joshyjoy999@gmail.com>
2020-03-18 18:33:34 +0530

    routes are added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,44 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { CasesComponent } from './cases/cases.component';
+import { CasesDetailsComponent } from './cases-details/cases-details.component';
+import { AddCasesComponent } from './add-cases/add-cases.component';
+import { CasesStatComponent } from './cases-stat/cases-stat.component';
+import { EditCasesComponent } from './edit-cases/edit-cases.component';
+
+const routes: Routes = [
+  {
+    path: 'cases',
+    component: CasesComponent,
+    data: { title: 'List of Cases'} 
+  },
+  {
+    path: 'case-details/:id',
+    component: CasesDetailsComponent,
+    data: { title: 'Case details'}
+  },
+  {
+    path: 'cases-stat',
+    component: CasesStatComponent,
+    data: { title: 'Cases Statistic'}
+  },
+  {
+    path: 'add-cases',
+    component: AddCasesComponent,
+    data: { title: 'Add cases'}
+  },
+  {
+    path: 'edit-cases',
+    component: EditCasesComponent,
+    data: { title: 'Edit cases'}
+  },
+  {
+    path:'',
+    redirectTo: '/cases',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
