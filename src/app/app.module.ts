c02f6d6fcb83161df02518fc1e552f416337fa7a
Joshy Joy <joshyjoy999@gmail.com>
2020-03-18 12:27:27 +0530

    5 new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CasesComponent } from './cases/cases.component';
+import { CasesDetailsComponent } from './cases-details/cases-details.component';
+import { AddCasesComponent } from './add-cases/add-cases.component';
+import { EditCasesComponent } from './edit-cases/edit-cases.component';
+import { CasesStatComponent } from './cases-stat/cases-stat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasesComponent,
+    CasesDetailsComponent,
+    AddCasesComponent,
+    EditCasesComponent,
+    CasesStatComponent
   ],
   imports: [
     BrowserModule,
