12cbea1c6896736e8baab415eead92f53b51daf7
Joshy Joy <joshyjoy999@gmail.com>
2020-03-19 15:29:37 +0530

    service created

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,10 +5,15 @@ import { catchError, tap, map } from 'rxjs/operators';
 import { Cases } from './cases';
 import { Statistic } from './statistic';
 
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = '/api/';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
